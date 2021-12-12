d42faf5ea753ec7d463ffb846fe1acc8fb699e93
Karim Rahal <karim@karimrahal.com>
2021-12-12 13:51:59 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="dns-exfil",
+    version="0.0.1",
+    author="karimpwnz",
+    author_email="karim@karimrahal.com",
+    description="Open a DNS server that knows no records but records every request.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karimpwnz/dns-exfil",
+    project_urls={
+        "Bug Tracker": "https://github.com/karimpwnz/dns-exfil/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: Internet :: Name Service (DNS)"
+    ],
+    packages=["dns_exfil"],
+    entry_points={"console_scripts": ["dns-exfil = dns_exfil.__main__:main"]},
+    python_requires=">=3.6",
+    install_requires=[
+        "dnslib >= 0.9.1"
+    ],
+)
